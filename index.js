434d6181f9e1605c243f3df0512ed7564c3cfb81
WebberWu <webber0928@users.noreply.github.com>
2021-06-14 16:53:42 +0800

    抓股票名稱

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 
 const request = require('request');
 const iconv = require('iconv-lite');
+const cheerio = require('cheerio');
 
 const FOREIGN_OVERBOUGHT_RANKING_URL = 'https://fubon-ebrokerdj.fbs.com.tw/Z/ZG/ZG_D.djhtm';
 
 request({url: FOREIGN_OVERBOUGHT_RANKING_URL, encoding: null}, function (error, response, body) {
-  console.error('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', iconv.decode(body, 'big5')); // Print the HTML for the Google homepage.
+    if (error)
+        console.error('error:', error);
+
+    if (response && response.statusCode !== 200)
+        console.log('statusCode:', response && response.statusCode); 
+    
+    let bodyHtml = iconv.decode(body, 'big5');
+    // console.log('body:', bodyHtml);
+    const $ = cheerio.load(bodyHtml);
+
+    let dataTable = $('table#oMainTable').html();
+    console.log('L20', dataTable)
+
+    let stock = [];
+
+    $('table#oMainTable td.t3t1').each(function(index, element){
+        stock[index] = {};
+        stock[index].name = $(this).text();
+    })
+
+    
+    console.log('L30',  stock)
 });
