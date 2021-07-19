459665333828420e0385c48b8e4fcf20978efaeb
WebberWu <webber0928@users.noreply.github.com>
2021-07-19 23:34:40 +0800

    Feature: 抓跌幅並且抓取KD 及 MACD

--- tammy.py
+++ tammy.py
@@ -1,8 +1,37 @@
 import requests
 import demjson
 from lxml import html
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
+import re
 from fake_useragent import UserAgent
+
+# 當日跌幅
+STOCK_DOWN_URL = 'https://fubon-ebrokerdj.fbs.com.tw/Z/ZG/ZG_AA.djhtm'
+stockDown = requests.get(STOCK_DOWN_URL, headers = {'user-agent': UserAgent().random})
+soup = BeautifulSoup(stockDown.text, 'html.parser')
+ele = soup.find_all('td', attrs={'class': 't3n0'})
+mainList = []
+for x in ele:
+    # print(x.text)
+    y = x.find_next_siblings()[0]
+    sid = y.find('a')['href'].split("javascript:Link2Stk('")[1].split("')")[0]
+    name = y.text[6:]
+    dealQty = x.find_next_siblings()[-1].text #.replace(',', '')
+    if len(x.find_next_siblings()) != 5:
+        mainList.append({
+            'sid': sid,
+            'name': name
+        })
+    else:
+        if int(dealQty.replace(',', '')) > 1000:
+            mainList.append({
+                'sid': sid,
+                'name': name
+            })
+
+print(mainList)
+print(len(mainList))
+
 
 ENTER_POINT = 'https://www.cmoney.tw/finance/technicalanalysis.aspx?s=2330'
 
@@ -12,8 +41,6 @@ result = session_requests.get(ENTER_POINT, headers = request_headers)
 
 
 # 抓取網頁的 cmkey
-import re
-from bs4 import BeautifulSoup
 soup = BeautifulSoup(result.content, 'html.parser')
 ele = soup.find('a', title=re.compile('技術分析'))
 
@@ -35,12 +62,13 @@ main = ['2611', '2613', '2617', '2637']
 # main = ['2611']
 sList = []
 
-for sid in main:
-    result = getStock(sid, cookie, cmkeyCode)
+for obj in mainList:
+    result = getStock(obj['sid'], cookie, cmkeyCode)
     jsonFormat = demjson.decode(result.content)
     # print(jsonFormat)
     # {'Date': '20210719', 'OpenPr': 35.9, 'HighPr': 37.7, 'LowPr': 34.25, 'ClosePr': 34.55, 'PriceDifference': -0.8, 'MagnitudeOfPrice': -2.26, 'MA5': 33.1, 'MA20': 35.4, 'MA60': 23.92, 'DealQty': 27467, 'K9': 37.47, 'D9': 31.98, 'DIF': 2.733, 'MACD': 3.776, 'DIF_MACD': -1.043, 'RSI5': 49.4, 'RSI10': 52.92}
     sList.append({
+        '名稱': obj['name'].replace("\n", "").replace("\r", "").replace(" ", ""),
         '日期': jsonFormat[0]['Date'],
         'K值': jsonFormat[0]['K9'],
         'D值': jsonFormat[0]['D9'],
@@ -50,5 +78,7 @@ for sid in main:
         '交易量': jsonFormat[0]['DealQty'],
         '五日線': jsonFormat[0]['MA5'],
     })
+
+
 
 print(sList)
